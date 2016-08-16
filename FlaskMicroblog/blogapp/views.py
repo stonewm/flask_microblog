b9a41f54e21d827e31cc16db0ccbe35c912e657a
stonewm <stone.wangmin@qq.com>
2016-08-16 22:16:07 +0800

    2016.8.16

--- FlaskMicroblog/blogapp/views.py
+++ FlaskMicroblog/blogapp/views.py
@@ -1,7 +1,42 @@
 # coding: utf8
 
 from blogapp import app
+from flask import render_template, redirect, flash
+from .forms import LoginForm
 
-@app.route('/')
+@app.route('/index')
 def index():
-    return '<h1>Hello World</h1>'
+    user = {'nickname': 'Stone'} # fake
+
+    # posts
+    posts = [
+        {
+            'author': {'nickname':'John'},
+            'body':'Beautiful day in Poland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The movie was so cool.'
+        }
+    ]
+    return render_template('index.html',   
+        title = 'Home',     
+        user = user,
+        posts = posts)
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        flash('Login required for openid:' + form.openid.data + \
+            ', remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
--- FlaskMicroblog/config.py
+++ FlaskMicroblog/config.py
@@ -1,2 +1,10 @@
 # coding: utf8
 
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+        {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+        {'name': 'Yahoo', 'url': 'https://me.yahoo.com' }
+]
+
